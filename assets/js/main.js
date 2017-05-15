71d3d7480955fb2e5a3b53790d0716f900e21ee5
Joao Pedro Costa Ferreiro <ferreiro@student.dei.uc.pt>
2017-05-15 02:29:18 +0100

    not bad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-//var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
 var game;
 
 // Create a new game instance 600px wide and 450px tall:
-game = new Phaser.Game(600, 450, Phaser.AUTO, '');
+
+game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example');
 
 // First parameter is how our state will be called.
 // Second parameter is an object containing the needed methods for state functionality
 game.state.add('Menu', Menu);
+game.state.add('Play', Play);
+//game.state.add('Game_Over', Game_Over);
 
 game.state.start('Menu');
